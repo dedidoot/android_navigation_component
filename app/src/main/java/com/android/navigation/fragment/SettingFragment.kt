dd3169dc17eee284faceab5358211a504dfda995
dedi <dedi@mamiteam.com>
2020-12-14 11:28:01 +0700

    setup

---
+++ app/src/main/java/com/android/navigation/fragment/SettingFragment.kt
@@ -0,0 +1,15 @@
+package com.android.navigation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.navigation.R
+
+class SettingFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_setting, container)
+    }
+}
